e49b0f6c5d177ee050f7c3a8eb59db7185a0f5e9
Lorence Lim <jlorencelim@gmail.com>
2019-01-04 20:34:03 +0800

    Rename project to stripe-app

--- stripe-app/wsgi.py
+++ stripe-app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lorencecrowdboticscom_l_1 project.
+WSGI config for lorence project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lorencecrowdboticscom_l_1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lorence.settings")
 
 application = get_wsgi_application()
